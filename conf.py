ea945279545ef290214df743c1e61c15773d7804
noviruszone <sukhmeeticonnect21@gmail.com>
2025-07-15 13:02:17 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'norton com setup'
 
 
 # The full version, including alpha/beta/rc tags
